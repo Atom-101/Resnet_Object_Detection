be0bb3d494290611a5dfd06ea2d5a30e12ccd733
Atmadeep Banerjee <atmadeepb@gmail.com>
2019-04-08 15:37:44 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -12,17 +12,13 @@ from tensorflow.keras import optimizers
 inputs = tf.keras.Input((416,416,3))
 model = Resnet18().build_model(inputs)
 
-valid_df = pd.read_csv('test.csv')
-# print(len(valid_df))
-# valid_df['image_name'] = '/home/atom/common_data/Projects/FlipkartGRID/Fastai/images/' + valid_df['image_name']
-print(valid_df.head())
+test_df = pd.read_csv('test.csv')
+# print(test_df.head())
 datagen = ImageDataGenerator()
-valid_generator = datagen.flow_from_dataframe(valid_df,directory='Fastai/images_test/',
+test_generator = datagen.flow_from_dataframe(test_df,directory='../Dataset/images_test/',
                     x_col = 'image_name',
-                    # y_col = ['x1','x2','y1','y2'],
                     y_col = None,
                     target_size=(416,416),batch_size=1,class_mode=None,shuffle=False)
-# print(next(valid_generator)[1])
 
 # def eval_loss(y_true,y_pred):
 #     # print(y_true.shape)
@@ -35,29 +31,21 @@ valid_generator = datagen.flow_from_dataframe(valid_df,directory='Fastai/images_
 
 model.load_weights('weights_52-1.000-0.794.h5')
 model.compile(optimizer='adam',loss="binary_crossentropy")
-# with open('model_architecture.json', 'w') as f:
-#     f.write(model.to_json())
 # print(model.summary())
 
-out = model.predict_generator(valid_generator,steps=len(valid_generator),use_multiprocessing=True,
+out = model.predict_generator(test_generator,steps=len(test_generator),use_multiprocessing=True,
                         workers=8,verbose=1)
 
 out = np.array(out)
+
+# Scale predictions to actual image size
 out[:,:2] *= 640
 out[:,2:] *= 480
 out = np.array(out,dtype=np.int32)
-valid_df['x1'] = out[:,0]
-valid_df['x2'] = out[:,1]
-valid_df['y1'] = out[:,2]
-valid_df['y2'] = out[:,3]
+test_df['x1'] = out[:,0]
+test_df['x2'] = out[:,1]
+test_df['y1'] = out[:,2]
+test_df['y2'] = out[:,3]
 
-valid_df.to_csv('Submission.csv',index=False)
-# model.evaluate_generator(valid_generator,steps=10,verbose=1)
-
-
-# with tf.Session() as sess:
-#     out = sess.run(tf.nn.sigmoid(out))
-
-
-
-
+test_df.to_csv('Final.csv',index=False)
+# model.evaluate_generator(test_generator,steps=10,verbose=1)
