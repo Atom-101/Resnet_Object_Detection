6687601fc327265f85b5559dee1e5e98a76886d6
Atmadeep Banerjee <atmadeepb@gmail.com>
2019-04-08 15:30:30 +0530

    Update main_tf_v2.py

--- Tensorflow 2/main_tf_v2.py
+++ Tensorflow 2/main_tf_v2.py
@@ -31,33 +31,6 @@ def loss(y_true,y_pred):
     l = 5*tf.sqrt(l)
     return l
 
-# def loss_iou(y_true,y_pred):
-# def loss_iou(y_true,y_pred):
-#     print(y_pred.shape)
-#     print(tf.minimum(y_true[:,0],y_pred[:,0]).shape)
-#     pt1 = tf.concat([tf.reshape(tf.minimum(y_true[:,0],y_pred[:,0]),(-1,1)),tf.reshape(tf.minimum(y_true[:,2],y_pred[:,2]),(-1,1))],axis=-1)
-#     pt2 = tf.concat([tf.reshape(tf.minimum(y_true[:,1],y_pred[:,1]),(-1,1)),tf.reshape(tf.minimum(y_true[:,3],y_pred[:,3]),(-1,1))],axis=-1)
-#     pt3 = tf.concat([tf.reshape(tf.maximum(y_true[:,0],y_pred[:,0]),(-1,1)),tf.reshape(tf.maximum(y_true[:,2],y_pred[:,2]),(-1,1))],axis=-1)
-#     pt4 = tf.concat([tf.reshape(tf.maximum(y_true[:,1],y_pred[:,1]),(-1,1)),tf.reshape(tf.maximum(y_true[:,3],y_pred[:,3]),(-1,1))],axis=-1) 
-#     print(pt3.shape)
-#     intersection = tf.reduce_prod(tf.abs(pt3-pt2),axis=-1)
-#     square1 = tf.reduce_prod(tf.abs(pt1-pt2),axis=-1)
-#     square2 = tf.reduce_prod(tf.abs(pt3-pt4),axis=-1)
-#     print(square1.shape)
-#     union = tf.abs(square1+square2-intersection)
-#     iou = tf.reduce_mean(intersection/union)
-# #     print(iou)
-
-#     l = tf.squared_difference(y_pred,y_true)
-#     l = tf.reduce_mean(l)
-#     l = 5*tf.sqrt(l)
-
-#     return 2*(1-iou)+l
-
-
-    
-
-
 inputs = tf.keras.Input((416,416,3))
 train_df = pd.read_csv('Train_Set.csv')
 valid_df = pd.read_csv('Valid_Set.csv')
@@ -68,15 +41,6 @@ train_generator = datagen.flow_from_dataframe(train_df,directory='../Dataset/ima
 valid_generator = datagen.flow_from_dataframe(valid_df,directory='..Dataset/images',
                     x_col = 'image_name',y_col = ['x1','x2','y1','y2'],
                     target_size=(416,416),batch_size=4,class_mode='other')
-
-# print(next(train_generator)[0].shape)
-
-# valid_x,valid_y=[],[]
-# for x,y in valid_generator:
-#     valid_x.append(x)
-#     valid_y.append(y)
-
-# valid_generator = datagen.flow(x=x,y=y,batch_size=4)
 
 
 model = Resnet18().build_model(inputs)
@@ -101,19 +65,4 @@ model.fit_generator(generator=train_generator,steps_per_epoch=200,
                     epochs=550,callbacks=callbacks_list,
                     use_multiprocessing=False,workers=0,initial_epoch=0)
 save_model(model,'IOU_Model.h5')
-# model.fit_generator(generator=train_generator,steps_per_epoch=200,
-#                     validation_data=valid_generator,validation_steps=10,
-#                     epochs=100,callbacks=callbacks_list,
-#                     use_multiprocessing=True,initial_epoch=50)
-# save_model(model,'Model.h5')
-# model.fit_generator(generator=train_generator,steps_per_epoch=200,
-#                     validation_data=valid_generator,validation_steps=10,
-#                     epochs=150,callbacks=callbacks_list,
-#                     use_multiprocessing=True,initial_epoch=100)
-# save_model(model,'Model.h5')
-# model.fit_generator(generator=train_generator,steps_per_epoch=200,
-#                     validation_data=valid_generator,validation_steps=10,
-#                     epochs=200,callbacks=callbacks_list,
-#                     use_multiprocessing=True,initial_epoch=150)
-# save_model(model,'Model.h5')
 
